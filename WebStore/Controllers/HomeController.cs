73b9268bceccefe390ed9abc8fe06384ad22dc9d
marga2002@yandex.ru <marga2002@yandex.ru>
2021-09-15 19:45:58 +0300

    Создание первого контроллера.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello from first easy controller");
+        }
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Second action with parameter {id}");
+        }
+    }
+}
