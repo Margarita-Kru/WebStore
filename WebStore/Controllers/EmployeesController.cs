b7402f7c3f7e4b62ff2f4fbfc824af12df2e5d26
marga2002@yandex.ru <marga2002@yandex.ru>
2021-09-21 18:25:20 +0300

    Изменение тестовых данных

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,19 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
+        public readonly IEnumerable<Employee> _Employees;
+
+        public EmployeesController()
         {
-            new Employee { Id = 1, FirstName = "Иван", LastName = "Самоедов", Patronymic = "Олегович", Age = 33 },
-            new Employee { Id = 2, FirstName = "Гоша", LastName = "Патрив", Patronymic = "Иванович", Age = 23 },
-            new Employee { Id = 3, FirstName = "Роман", LastName = "Ростков", Patronymic = "Витальевич", Age = 35 },
-            new Employee { Id = 4, FirstName = "Мария", LastName = "Мирошкина", Patronymic = "Григорьевна", Age = 29 },
-            new Employee { Id = 5, FirstName = "Алексей", LastName = "Морозов", Patronymic = "Владимирович", Age = 63 },
-        };
+            _Employees = TestData.Employees;
+        }
         public IActionResult Index()
         {
             return View(_Employees);
