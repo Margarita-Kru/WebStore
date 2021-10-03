ec3a88ef0a327027493b0da028d8556c9f7531d6
marga2002@yandex.ru <marga2002@yandex.ru>
2021-10-03 17:49:36 +0300

    Логика каталога товаров и товаров, фильтр товаров

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -8,6 +10,18 @@ namespace WebStore.Services
     public class InMemoryProductData : IProductData
     {
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            IEnumerable<Product> querry = TestData.Products;
+
+            if (Filter?.SectionId != null)
+                querry = querry.Where(p => p.SectionId == Filter.SectionId);
+
+            if (Filter?.BrandId != null)
+                querry = querry.Where(p => p.BrandId == Filter.BrandId);
+            return querry;
+        }
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
     }
