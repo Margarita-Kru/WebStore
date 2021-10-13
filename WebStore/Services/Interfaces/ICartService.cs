4fc54abed3d7db5fc61b7bd1cbf9c3f9c24d43a2
marga2002@yandex.ru <marga2002@yandex.ru>
2021-10-13 15:34:58 +0300

    Описание интерфейса сервиса корзины

---
+++ WebStore/Services/Interfaces/ICartService.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.ViewModels;
+
+namespace WebStore.Services.Interfaces
+{
+    interface ICartService
+    {
+        void Add(int Id);
+        void Decrement(int Id);
+        void Remove(int Id);
+        void Clear();
+        CartViewModel GetViewModel();
+    }
+}
