3369a3a31c0f068fb8420a1092af80e466f22f1d
marga2002@yandex.ru <marga2002@yandex.ru>
2021-10-25 21:39:52 +0300

    Проектирование WebAPI для управления сотрудниками

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -29,7 +29,25 @@ namespace WebStore
         }
         public void ConfigureServices(IServiceCollection services)
         {
-                    services.AddIdentity<User, Role>()
+            var database_type = Configuration["Database"];
+
+            switch (database_type)
+            {
+                default: throw new InvalidOperationException($"Тип БД {database_type} не поддерживается");
+
+                case "SqlServer":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlServer(Configuration.GetConnectionString(database_type)));
+                    break;
+
+                case "Sqlite":
+                    services.AddDbContext<WebStoreDB>(opt =>
+                        opt.UseSqlite(Configuration.GetConnectionString(database_type),
+                            o => o.MigrationsAssembly("WebStore.DAL.Sqlite")));
+                    break;
+            }
+
+            services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<WebStoreDB>()
                 .AddDefaultTokenProviders();
 
