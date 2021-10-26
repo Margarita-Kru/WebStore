b1166df8304b50f4c87cca95dd4ac8fa19b38407
marga2002@yandex.ru <marga2002@yandex.ru>
2021-10-26 13:11:16 +0300

    Заготовка контроллера WebAPI-сервиса заказов

---
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersApiController : ControllerBase
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet("user/{UserName}")]
+        public async Task<IActionResult> GetUserOrders(string UserName)
+        {
+            var orders = await _OrderService.GetUserOrders(UserName);
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById(int id)
+        {
+            var order = await _OrderService.GetOrderById(id);
+            if (order is null)
+                return NotFound();
+            return Ok(order);
+        }
+
+        [HttpPost("{UserName}")]
+        public async Task<IActionResult> CreateOrder(string UserName)
+        {
+            var order = await _OrderService.CreateOrder(UserName,);
+            return Ok(order);
+        }
+    }
+}
