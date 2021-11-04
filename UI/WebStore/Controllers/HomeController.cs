6b6c6ad8d987ceb026db538b0547361a32694be9
marga2002@yandex.ru <marga2002@yandex.ru>
2021-11-04 17:34:11 +0300

    Тестирование HomeController

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
-        
+
         public IActionResult Status( string id)
         {
-            switch(id)
+            if (id is null)
+                throw new ArgumentNullException(nameof(id));
+
+            switch (id)
             {
                 default: return Content($"Status code - {id}");
                 case "404": return View("Error");
