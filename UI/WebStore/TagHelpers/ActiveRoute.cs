6700ca2246919713a22139d30242ef93dcb08dc8
marga2002@yandex.ru <marga2002@yandex.ru>
2021-11-08 15:15:57 +0300

    Данные, необходимые для работы таг-хелпера

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebStore.TagHelpers
 {
@@ -10,6 +10,18 @@ namespace WebStore.TagHelpers
     public class ActiveRoute:TagHelper
     {
         private const string AttributeName = "ws-is-active-route";
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RouteValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
